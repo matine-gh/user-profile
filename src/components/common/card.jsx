5ea0e06fbac924b395fb91b33ef0b837a4273e2d
matine-gh <matine.ghazanfari@gmail.com>
2023-04-25 19:42:53 +0330

    [feat]/create card component

---
+++ src/components/common/card.jsx
@@ -0,0 +1,12 @@
+export default function Card(props) {
+    const {cardData} = props;
+    return (
+        <a key={cardData.title} href={cardData.href}
+           className={`${cardData.backgroundColor} ${cardData.boxShadow} 
+                                    inline-flex items-center justify-center m-4 rounded-xl
+                                    text-2xl text-dark-gray font-extrabold cursor-pointer`}
+        >
+            <p>{cardData.title}</p>
+        </a>
+    );
+}
--- src/components/home/mainBody.jsx
+++ src/components/home/mainBody.jsx
@@ -1,21 +1,17 @@
+import Card from "@/components/common/card";
+
 export default function MainBody() {
 
     const items = [
-        {title: 'Introduction', backgroundColor: 'bg-light-rose', boxShadow: 'hover:shadow-rose'},
-        {title: 'Panel Services', backgroundColor: 'bg-light-orange', boxShadow: 'hover:shadow-orange', href: '/usersList'},
-        {title: 'Notification', backgroundColor: 'bg-light-lime', boxShadow: 'hover:shadow-lime'},
-        {title: 'Registration Statics', backgroundColor: 'bg-light-indigo', boxShadow: 'hover:shadow-indigo'},
+        {title: 'Introduction', key: 'introduction', backgroundColor: 'bg-light-rose', boxShadow: 'hover:shadow-rose'},
+        {title: 'Panel Services', key: 'panelServices', backgroundColor: 'bg-light-orange', boxShadow: 'hover:shadow-orange', href: '/usersList'},
+        {title: 'Notification', key: 'notification', backgroundColor: 'bg-light-lime', boxShadow: 'hover:shadow-lime'},
+        {title: 'Registration Statics', key: 'registrationStatics', backgroundColor: 'bg-light-indigo', boxShadow: 'hover:shadow-indigo'},
     ];
     return (
         <div className={"flex-1 grid grid-cols-content grid-rows-content justify-center content-center max-w-6xl mx-auto"}>
             {items.map((item) =>
-                    <a key={item.title} href={item.href}
-                         className={`${item.backgroundColor} ${item.boxShadow} 
-                                    inline-flex items-center justify-center m-4 rounded-xl
-                                    text-2xl text-dark-gray font-extrabold cursor-pointer`}
-                    >
-                        <p>{item.title}</p>
-                    </a>
+                    <Card key={item.key} cardData={item}/>
                 )}
         </div>
     );
