b734657ee6462f840335237af7555dfb07cade4a
matine-gh <matine.ghazanfari@gmail.com>
2023-04-25 19:25:12 +0330

    [fix]/condition on login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,13 +1,23 @@
 import LoginForm from "@/components/login/loginForm";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
-
-const containerStyles = "flex flex-col items-center justify-center " +
-    "h-screen " +
-    "text-lighter-gray " +
-    "bg-gradient-to-b from-primary-gray to-dark-gray";
-const headingStyles = "mb-12 text-primary-green text-6xl";
 
 export default function Login() {
+    const containerStyles = "flex flex-col items-center justify-center " +
+        "h-screen " +
+        "text-lighter-gray " +
+        "bg-gradient-to-b from-primary-gray to-dark-gray";
+    const headingStyles = "mb-12 text-primary-green text-6xl";
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if(localStorage.getItem('loginData') !== null){
+            router.replace('/home')
+        }
+    }, [])
+
     return (
         <div className={containerStyles}>
             <h1 className={headingStyles}>Sign in</h1>
