5776eed7ecc46ae47556b03c8c2273b7a9a440ef
matine-gh <matine.ghazanfari@gmail.com>
2023-04-27 12:40:20 +0330

    [feat]/create card component

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -1,7 +1,7 @@
 export default function Card(props) {
     const {cardData} = props;
     return (
-        <a key={cardData.title} href={cardData.href}
+        <a key={cardData.title} href={cardData.href || null}
            className={`${cardData.backgroundColor} ${cardData.boxShadow} 
                                     inline-flex items-center justify-center m-4 rounded-xl
                                     text-2xl text-dark-gray font-extrabold cursor-pointer`}
